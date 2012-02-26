ccc7bb3d174497211bd515de38aae7433ddaf498
Marco Rondini <white_sheep@ihteam.net>
2012-02-26 06:03:13 -0800

    resync

--- streams/sample.js
+++
@@ -1,9 +0,0 @@
-var LogLizer_stream = function(conf){
-	this.conf = conf;
-}
-
-LogLizer_stream.prototype.stream = function(conf, data){
-	console.log(data);
-}
-
-exports.LogLizer_stream = LogLizer_stream;
