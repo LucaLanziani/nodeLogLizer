998c88c6880e4ac2e7436992bb2810c23e670bec
Marco Rondini <white_sheep@ihteam.net>
2012-03-05 03:18:32 -0800

    aggiunto vero salvataggio delle configurazioni + restart di tutta l'applicazione

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-require("./lib/extends.js");														// require extends 
+require("./lib/extends");														// require extends 
 
 global.LLconf_path = __dirname + "/conf.json";
 
 /* globals */
-global.LogLizer_regexp 	= require("./lib/loglizer-regexp.js").LogLizer_regexp;		// require extended regexp class
+global.LLversion = "0.1a";
+global.LogLizer_regexp 	= require("./lib/loglizer-regexp").LogLizer_regexp;		// require extended regexp class
 global.LLconf 			= require(LLconf_path).clone();								// require configuration from conf.js
 /* end globals require */
 
 /* not globals */
-var LogLizer_core 		= require("./lib/loglizer-core.js").LogLizer_core;			// require core class
-var LogLizer_web 		= require("./lib/loglizer-web.js").LogLizer_web;			// require web class
-var LogLizer_utils 		= require("./lib/loglizer-utils.js").LogLizer_utils;		// require utils class
-var LogLizer_streams 	= require("./lib/loglizer-streams.js").LogLizer_streams;	// require streams class
-var LogLizer_db 		= require("./lib/loglizer-db.js").LogLizer_db;				// require streams class
+var LogLizer_core 		= require("./lib/loglizer-core").LogLizer_core;			// require core class
+var LogLizer_web 		= require("./lib/loglizer-web").LogLizer_web;			// require web class
+var LogLizer_utils 		= require("./lib/loglizer-utils").LogLizer_utils;		// require utils class
+var LogLizer_streams 	= require("./lib/loglizer-streams").LogLizer_streams;	// require streams class
+var LogLizer_db 		= require("./lib/loglizer-db").LogLizer_db;				// require streams class
 /* end local require */
 
 /* global instance */
@@ -20,12 +21,16 @@ global.LLdb 			= new LogLizer_db(LLconf.globals.mongodb);
 global.LLutils 			= new LogLizer_utils();										// global instance for utils
 global.LLweb 			= new LogLizer_web(LLconf.globals);							// global instance for web server
 global.LLstreams 		= new LogLizer_streams(LLconf.globals);						// global instance for streams
+global.LLcores 			= [];	
 /* end global instance */
 
-LLconf.version = "0.1a";
-LLweb.start();
+ret = LLutils.check_conf(LLconf);
+if ( ret.error ){
+	throw "conf.json error: " + ret.type;
+}
 
-var LLcores = [];																
+LLweb.start();
+															
 for ( i in LLconf.logs ){																		
 	LLcores[i] = new LogLizer_core(LLconf.globals, LLconf.logs[i]);					// each configuration, create new core instance 
 	LLcores[i].main();																// launch core main 
